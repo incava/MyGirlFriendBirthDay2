619c16bc18bcd875fe7cf95f1c4fc0f168fce870
incava <68988975+incava@users.noreply.github.com>
2023-06-16 21:21:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mygirlfriendbirthday2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mygirlfriendbirthday2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mygirlfriendbirthday2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    //
+
+}
